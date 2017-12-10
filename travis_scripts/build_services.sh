3a581aa9188daeaab9718397fc2aa43516cdd480
Yevhen Pekhulia <pehulja@gmail.com>
2017-12-10 17:50:18 +0100

    Modified credentials and other settings

--- travis_scripts/build_services.sh
+++ travis_scripts/build_services.sh
@@ -1,2 +1,3 @@
+#!/usr/bin/env bash
 echo "Building with travis commit of $BUILD_NAME ..."
 mvn clean package docker:build
--- travis_scripts/deploy_to_amazon_ecs.sh
+++ travis_scripts/deploy_to_amazon_ecs.sh
@@ -1,4 +1,7 @@
+#!/usr/bin/env bash
 echo "Launching $BUILD_NAME IN AMAZON ECS"
-ecs-cli configure --region us-west-1 --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY --cluster spmia-tmx-dev
+
+ecs-cli configure profile --profile-name profile_name --access-key $AWS_ACCESS_KEY --secret-key $AWS_SECRET_KEY
+ecs-cli configure --cluster spmia-tmx-dev --region us-east-2
 ecs-cli compose --file docker/common/docker-compose.yml up
 rm -rf ~/.ecs
